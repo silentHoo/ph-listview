a0b3a715d05044d22fbef7ba7bf7f1a3650369e7
Patrick Hillert <patrick.hillert@inovex.de>
2015-11-22 17:02:25 +0100

    Move folder and delete temp files

--- tmp-1448207570038/components/ph-listview/wct.conf.js
+++
@@ -1,8 +0,0 @@
-//overwriting the default-conf to prevent safari from running (no safari 9.0-driver available at the moment)
-module.exports = {
-  plugins: {
-    local: {
-      browsers: ['firefox']
-    }
-  },
-};
